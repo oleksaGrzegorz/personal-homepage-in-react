5128c82cf5c2a6ab87b428519a8dadfd1300fc73
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-04-24 18:07:05 +0200

    remove logo and text

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        DARK MODE OFF <button>przycisk</button>
+      </p>
+      <p>"photo"</p>
+      <p>THIS IS</p>
+      <p>Grzegorz Oleksa</p>
+      <p>
+        I'm a passionate Frontend Developer in love with React, currently
+        looking for new job opportunities.
+      </p>
+      <button>"ikonka wiadomosci" Hire Me</button>
+
+      <section className="section">
+        <h2 className="header2">My skillset includes 🛠️</h2>
+        <ul>
+          <li>Semantic & accessible HTML</li>
+          <li>Responsive Web Design</li>
+          <li>Teamwork</li>
+          <li>Markdown</li>
+          <li>Immutability</li>
+          <li>CSS BEM convention</li>
+          <li>CSS Grid</li>
+          <li>CSS Flexbox</li>
+          <li>React Router</li>
+          <li>Redux-Saga</li>
+          <li>Redux (Toolkit)</li>
+          <li>React Hooks</li>
+          <li>Error handling</li>
+          <li>Working with API (fetch, axios)</li>
+          <li>JavaScript: ES6+</li>
+          <li>Promises, Async/Await</li>
+          <li>GitHub Pull Requests & Review</li>
+          <li>Trello</li>
+          <li>Scrum</li>
+          <li>NPM</li>
+          <li>React</li>
+          <li>Git</li>
+        </ul>
+      </section>
+
+      <section>
+        <p>What I want to learn next "ikonka rakiety"</p>
+        <p>"wymienione skille"</p>
+      </section>
+      <section>
+        <p>"ikonka githuba"</p>
+        <p>Portfolio</p>
+        <p>My recent projects</p>
+        <p>"tu miejsce na moje projekty z githuba"</p>
+      </section>
+      <section>
+        <p>LET'S TALK!</p>
+        <p>grzesiek@gmail.com</p>
+        <p>"tekst o mnie"</p>
+        <p>"ikonka github" "facebook" "linkedin" "insta"</p>
+      </section>
     </div>
   );
 }

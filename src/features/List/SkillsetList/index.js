5b12b97c7b2898718a4e067a45f3dda116f83f54
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-04-27 16:51:59 +0200

    created components for features List

---
+++ src/features/List/SkillsetList/index.js
@@ -0,0 +1,30 @@
+import { List, ListItem } from "../styled";
+
+const SkillsetList = () => (
+  <List>
+    <ListItem>Semantic & accessible HTML</ListItem>
+    <ListItem>Responsive Web Design</ListItem>
+    <ListItem>GitHub Pull Requests & Review</ListItem>
+    <ListItem>Markdown</ListItem>
+    <ListItem>Immutability</ListItem>
+    <ListItem>CSS BEM convention</ListItem>
+    <ListItem>CSS Grid</ListItem>
+    <ListItem>CSS Flexbox</ListItem>
+    <ListItem>React Router</ListItem>
+    <ListItem>Redux-Saga</ListItem>
+    <ListItem>Redux (Toolkit)</ListItem>
+    <ListItem>React Hooks</ListItem>
+    <ListItem>Error handling</ListItem>
+    <ListItem>Working with API (fetch, axios)</ListItem>
+    <ListItem>JavaScript: ES6+</ListItem>
+    <ListItem>Promises, Async/Await</ListItem>
+    <ListItem>GitHub Pull Requests & Review</ListItem>
+    <ListItem>Trello</ListItem>
+    <ListItem>Scrum</ListItem>
+    <ListItem>NPM</ListItem>
+    <ListItem>React</ListItem>
+    <ListItem>Git</ListItem>
+  </List>
+);
+
+export default SkillsetList;
---
+++ src/features/List/WhatToLearList/index.js
@@ -0,0 +1,15 @@
+import { List, ListItem } from "../styled";
+
+const WhatToLearnList = () => (
+  <List>
+    <ListItem>React Context</ListItem>
+    <ListItem>Cypress</ListItem>
+    <ListItem>TypeScript</ListItem>
+    <ListItem>Node.js</ListItem>
+    <ListItem>Unit testing</ListItem>
+    <ListItem>JS classes</ListItem>
+
+  </List>
+);
+
+export default WhatToLearnList;

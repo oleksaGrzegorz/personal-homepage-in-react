2ef31b1f059507e0a28f62faa3a4479948d2a3f9
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-04-28 17:13:50 +0200

    added media queries

--- src/features/List/styled.js
+++ src/features/List/styled.js
@@ -6,21 +6,31 @@ export const List = styled.ul`
   list-style: none;
   padding: 0;
   gap: 8px 0;
-  justify-content: space-between;
+  justify-content: flex-start; /* lepiej */
 `;
 
 export const ListItem = styled.li`
-  width: 33%;
+  flex-basis: calc(100% / 3);
+  min-width: 0;
   font-family: "Inter", sans-serif;
   font-weight: 400;
   font-size: 18px;
   line-height: 140%;
   letter-spacing: 5%;
   color: rgba(110, 126, 145, 1);
+
   &::before {
     content: "•";
     color: rgba(3, 102, 214, 1);
     margin-right: 0.5rem;
     font-size: 30px;
+  }
+
+  @media (max-width: 768px) {
+    flex-basis: 50%;
+  }
+
+  @media (max-width: 480px) {
+    flex-basis: 100%;
   }
 `;

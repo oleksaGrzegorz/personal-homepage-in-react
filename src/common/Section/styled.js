30d256619928c4dc5a12c15d6c4274f34ed0e435
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-05-17 13:06:59 +0200

    changed styled for sections

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -4,33 +4,31 @@ export const StyledSection = styled.section`
   background-color: White;
   box-sizing: border-box;
   width: calc(100% - 32px);
-  margin-left: 16px;
-  margin-right: 16px;
   max-width: 1216px;
-  margin: 0 auto;
   padding: 32px;
   box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03);
   margin-top: 72px;
-
+  border-radius: 4px;
 `;
 
-
 export const Header = styled.h2`
-  font-family: Inter;
   line-height: 100%;
   border-bottom: 1px solid rgba(209, 213, 218, 0.3);
   padding-bottom: 13px;
   font-weight: 900;
   font-size: 30px;
-  letter-spacing: 5%;
+  letter-spacing: 0.05em;
   margin-top: 0;
+  margin-bottom: 32px;
 `;
 
 export const HeaderIcon = styled.span`
-  font-family: Inter;
   font-weight: 400;
   font-size: 24px;
-  line-height: 100%;
-  letter-spacing: 5%;
+  line-height: 1;
+  letter-spacing: 0.05em;
   text-align: center;
+  width: 27px;
+  height: 32px;
+  display: inline-block;
 `;

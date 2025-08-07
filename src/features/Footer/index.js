b3ed2d289ab1caef5e9889269782432bff9728f1
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-08-07 19:55:23 +0200

    removed react.fragment from footer

--- src/features/Footer/index.js
+++ src/features/Footer/index.js
@@ -12,21 +12,19 @@ import {
 } from "./styled";
 
 export const Footer = ({ isDarkMode }) => (
-  <>
-    <Section>
-      <ContentWrapper>
-        <SmallParagraph>LET'S TALK!</SmallParagraph>
-        <EmailParagraph>grzesiek@gmail.com</EmailParagraph>
-        <MidParagraph>
-          I’m always open to new projects whenever I have the time. If you have
-          a website, dashboard or mobile app in mind and need some help to make
-          your ideas come to life, feel free to contact me. ✌️
-        </MidParagraph>
-        <StyledDiv isDarkMode={isDarkMode}>
-          <StyledGitHubIcon /> <StyledFacebookIcon /> <StyledLinkedinIcon />{" "}
-          <StyledInstagramIcon />
-        </StyledDiv>
-      </ContentWrapper>
-    </Section>
-  </>
+  <Section>
+    <ContentWrapper>
+      <SmallParagraph>LET'S TALK!</SmallParagraph>
+      <EmailParagraph>grzesiek@gmail.com</EmailParagraph>
+      <MidParagraph>
+        I’m always open to new projects whenever I have the time. If you have a
+        website, dashboard or mobile app in mind and need some help to make your
+        ideas come to life, feel free to contact me. ✌️
+      </MidParagraph>
+      <StyledDiv isDarkMode={isDarkMode}>
+        <StyledGitHubIcon /> <StyledFacebookIcon /> <StyledLinkedinIcon />{" "}
+        <StyledInstagramIcon />
+      </StyledDiv>
+    </ContentWrapper>
+  </Section>
 );
